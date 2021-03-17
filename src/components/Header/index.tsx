0f463283bd4daffe43a2e7fd90b4be5cf3d10b21
Marcos Daniel <marcos.mdbbr@gmail.com>
2021-03-17 06:10:22 -0300

    alterando css do modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,49 +1,19 @@
 import { useState } from 'react';
 import logoImg from '../../assets/logo.svg';
 import { Container, Content } from './styles';
-import Modal from 'react-modal';
 
-const customStyles = {
-    content: {
-        top: '50%',
-        left: '50%',
-        right: 'auto',
-        bottom: 'auto',
-        marginRight: '-50%',
-        transform: 'translate(-50%, -50%)'
-    }
-};
+interface HeaderProps{
+    onOpenNewTransactionModal: () => void;
+}
 
-
-export function Header() {
-
-    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
-
-    function handleOpenNewTransactionModal() {
-        setIsNewTransactionModalOpen(true);
-    }
-
-    function handleCloseNewTransactionModal() {
-        setIsNewTransactionModalOpen(false);
-    }
-
+export function Header({onOpenNewTransactionModal}: HeaderProps) {
     return (
-
         <Container>
-
             <Content>
                 <img src={logoImg} alt="dt money" />
-                <button type="button" onClick={handleOpenNewTransactionModal}>
+                <button type="button" onClick={onOpenNewTransactionModal}>
                     Nova transação
             </button>
-
-                <Modal
-                    isOpen={isNewTransactionModalOpen}
-                    onRequestClose={handleCloseNewTransactionModal}
-                    style={customStyles}
-                    contentLabel="Example Modal">
-                    <h1>Cadastrar transação</h1>
-                </Modal>
             </Content>
         </Container>
     );
